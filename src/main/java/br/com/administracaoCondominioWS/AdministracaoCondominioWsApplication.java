a503a641e5a8e974a11718a13e18497767770144
Mauricio Carvalho <mauricio.cac@hotmail.com>
2022-01-14 11:57:53 -0300

    Deploy no heroku

---
+++ src/main/java/br/com/administracaoCondominioWS/AdministracaoCondominioWsApplication.java
@@ -0,0 +1,13 @@
+package br.com.administracaoCondominioWS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdministracaoCondominioWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdministracaoCondominioWsApplication.class, args);
+	}
+
+}
---
+++ src/main/java/br/com/administracaoCondominioWS/model/dto/MoradorDTO.java
@@ -0,0 +1,49 @@
+package br.com.administracaoCondominioWS.model.dto;
+
+public class MoradorDTO {
+	private Long id;
+	private String nome;
+	private String cpf;
+	private String email;
+
+	public MoradorDTO(Long id, String nome, String cpf, String email) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.cpf = cpf;
+		this.email = email;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
